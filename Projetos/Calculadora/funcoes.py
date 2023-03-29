f2181ed357e06271672b6decfcf52bd3d98a63ad
Gabriel Mourão <gb.mourao@hotmail.com>
2023-03-29 19:42:14 -0300

    Commit da função subtracao

--- Projetos/Calculadora/funcoes.py
+++ Projetos/Calculadora/funcoes.py
@@ -2,3 +2,8 @@ def soma (a, b):
     if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
         print("Os números digitados devem ser int ou float")
     return a + b
+
+def subtracao (a, b):
+    if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
+        print("Os números digitados devem ser int ou float")
+    return a - b
