097b97f6b9f4835558deaca891415ac2e8958aa7
Gabriel Mourão <gb.mourao@hotmail.com>
2023-03-29 19:49:08 -0300

    Commit da função divisao

--- Projetos/Calculadora/funcoes.py
+++ Projetos/Calculadora/funcoes.py
@@ -5,5 +5,12 @@ def soma (a, b):
 
 def subtracao (a, b):
     if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
-        print("Os números digitados devem ser int ou float")
+        print("Os númegros digitados devem ser int ou float")
     return a - b
+
+def divisao (a, b):
+    if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
+        print("Os númegros digitados devem ser int ou float")
+    elif b == 0:
+        print("Erro: divisão por zero")
+    return a/b
